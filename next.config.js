fe52d55d445d55cdef20a943ab13f4ed648436f1
lokeshpara <lokeshpara17@gmail.com>
2025-04-23 18:11:38 -0500

    Update scripts to use environment variable for repository name

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 const nextConfig = {
-  output: 'export',
-  // Use correct basePath for GitHub Pages
-  basePath: '/-',
-  assetPrefix: '/-/',
+  output: "export",
+  // Remove the repository name from the path for cleaner URLs
+  basePath: basePath,
+  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
